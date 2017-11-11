01c5d87acd515608e306ae73bfd1731943f8de1c
Alan WL Chan <akirabbq@gmail.com>
2017-11-11 17:37:27 +0800

    yeah

--- hscscommon.go
+++ hscscommon.go
@@ -2,7 +2,9 @@ package hld
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"unicode/utf16"
 	"unicode/utf8"
@@ -15,19 +17,19 @@ const BOM_UTF8 = 1
 const BOM_UTF16LE = 2
 const BOM_UTF16BE = 3
 
-//GetUTFBomType check if bytes have the UTF8 BOM mark
-func GetUTFBomType(bytes []byte) int {
+//GetUTFBomType check if bytes have the UTF8 BOM mark, and return BOM len
+func GetUTFBomType(bytes []byte) (int, int) {
 	// if len(*bytes) < 3 {
 	// 	return false
 	// }
 	if (len(bytes) >= 3) && (bytes[0] == 239) && (bytes[1] == 187) && (bytes[2] == 191) {
-		return BOM_UTF8
+		return BOM_UTF8, 3
 	} else if (len(bytes) >= 2) && (bytes[0] == 255) && (bytes[1] == 254) {
-		return BOM_UTF16LE
+		return BOM_UTF16LE, 2
 	} else if (len(bytes) >= 2) && (bytes[0] == 254) && (bytes[1] == 255) {
-		return BOM_UTF16BE
+		return BOM_UTF16BE, 2
 	}
-	return 0
+	return 0, 0
 
 }
 
@@ -51,8 +53,8 @@ func SameFile(filename1, filename2 string) bool {
 	return os.SameFile(f1, f2)
 }
 
-//DecodeUTF16 decode UTF16 string UTF8
-func DecodeUTF16(b []byte) (string, error) {
+//DecodeUTF16 decode UTF16 string UTF8, assume little indian and no BOM mark
+func DecodeUTF16(b []byte, order binary.ByteOrder) (string, error) {
 
 	if len(b)%2 != 0 {
 		return "", fmt.Errorf("Must have even length byte slice")
@@ -66,18 +68,43 @@ func DecodeUTF16(b []byte) (string, error) {
 
 	lb := len(b)
 	for i := 0; i < lb; i += 2 {
-		u16s[0] = uint16(b[i]) + (uint16(b[i+1]) << 8)
+
+		u16s[0] = order.Uint16(b[i:])
 
 		if u16s[0] >= 0xD800 && u16s[0] <= 0xE000 {
 			i += 2
-			u16s[1] = uint16(b[i]) + (uint16(b[i+1]) << 8)
+			u16s[1] = order.Uint16(b[i:])
 		}
 
 		r := utf16.Decode(u16s)
-
 		n := utf8.EncodeRune(b8buf, r[0])
 		ret.Write(b8buf[:n])
 	}
 
 	return ret.String(), nil
+}
+
+//HSFileToString read text file into strings
+func HSFileToString(filename string) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
+
+	bomtype, bomlen := GetUTFBomType(data)
+	switch bomtype {
+	case BOM_UTF8:
+		return string(data[bomlen:]), nil
+	case BOM_UTF16LE:
+		return DecodeUTF16(data[bomlen:], binary.LittleEndian)
+	case BOM_UTF16BE:
+		return DecodeUTF16(data[bomlen:], binary.BigEndian)
+	default:
+		return string(data), nil
+		//log.Println("default")
+	}
 }

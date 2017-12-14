f466721b9b1c36fa6cdf4f54fe20a785f8d6fd65
edpcwlc <wl.chan@hld.com>
2017-12-14 17:28:52 +0800

    add progress AES encryption

--- hscrypto.go
+++ hscrypto.go
@@ -7,6 +7,7 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"errors"
+	"fmt"
 	"log"
 )
 
@@ -51,8 +52,8 @@ func GenerateAESKeys(strength int) ([]byte, []byte) {
 	copy(key, sha[0:strength])
 	bytes, _ = GenerateRandomBytes(64)
 	sha = sha256.Sum256(bytes)
-	iv := make([]byte, 16)
-	copy(iv, sha[0:16])
+	iv := make([]byte, aes.BlockSize)
+	copy(iv, sha[0:aes.BlockSize])
 	return key, iv
 }
 
@@ -88,4 +89,37 @@ func DecryptAES(key []byte, iv []byte, ciphertext []byte) ([]byte, error) {
 		return padded, nil
 	}
 	return nil, err
+}
+
+//AESStartEncrypt start AES progressive encryption
+func AESStartEncrypt(key []byte, iv []byte) (cipher.BlockMode, error) {
+	var err error
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	return cipher.NewCBCEncrypter(block, iv), nil
+}
+
+//AESSSContEncrypt continue progressive encrypt
+func AESSSContEncrypt(blockMode cipher.BlockMode, data []byte) ([]byte, error) {
+	if (len(data) % aes.BlockSize) != 0 {
+		return nil, fmt.Errorf("data must be in %d bytes block", aes.BlockSize)
+	}
+	cipherText := make([]byte, len(data))
+	blockMode.CryptBlocks(cipherText, data)
+	return cipherText, nil
+}
+
+//AESEndEncrypt End the progressive encrypt
+func AESEndEncrypt(blockMode cipher.BlockMode, data []byte) ([]byte, error) {
+	if len(data) == 0 {
+		return nil, fmt.Errorf("cannot end with empty data")
+	}
+
+	padded := PKCS5Padding(data, aes.BlockSize)
+	cipherText := make([]byte, len(padded))
+	blockMode.CryptBlocks(cipherText, padded)
+
+	return cipherText, nil
 }
